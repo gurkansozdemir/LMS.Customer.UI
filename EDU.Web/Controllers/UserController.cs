4c466e0e0b8a75306a6cc5d5c6931019d38fd46d
Gurkan Sozdemir <gurkansozdemir@hotmail.com.tr>
2022-07-02 23:30:03 +0300

    Initialize Commit

---
+++ EDU.Web/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EDU.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult All()
+        {
+            return View();
+        }
+    }
+}
---
+++ EDU.Web/wwwroot/assets/js/UserScript/UserScript.js
@@ -0,0 +1,16 @@
+﻿function getAllUser() {
+    var content = "";
+    $.ajax({
+        type: "GET",
+        url: "https://localhost:7263/api/user",
+        dataType: "json",
+        success: function (response) {
+            debugger
+            content = `<ul><li>` + response.data[0].firstName + ` ` + response.data[0].lastName + `</li></ul>`;
+            $("#userList").append(content);
+        },
+        error: function (error) {
+
+        }
+    });
+}
